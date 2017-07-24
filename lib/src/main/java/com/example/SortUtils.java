62fed5781fc01746d034f2e28605df5997fd4795
yajun.sun <yajun.sun@cootek.cn>
2017-07-24 20:52:22 +0800

    添加了选择排序

--- lib/src/main/java/com/example/SortUtils.java
+++ lib/src/main/java/com/example/SortUtils.java
@@ -62,4 +62,27 @@ public class SortUtils {
                 quickSort(numbers, i, end);
         }
     }
+
+    /**
+     * 选择排序<br/>
+     * <li>在未排序序列中找到最小元素，存放到排序序列的起始位置</li>
+     * <li>再从剩余未排序元素中继续寻找最小元素，然后放到排序序列末尾。</li>
+     * <li>以此类推，直到所有元素均排序完毕。</li>
+
+     *
+     * @param numbers
+     */
+    public static void selectSort(int[] numbers) {
+        int size = numbers.length, temp;
+        for (int i = 0; i < size; i++) {
+            int k = i;
+            for (int j = size - 1; j >i; j--)  {
+                if (numbers[j] < numbers[k])  k = j;
+            }
+            temp = numbers[i];
+            numbers[i] = numbers[k];
+            numbers[k] = temp;
+        }
+    }
+
 }
